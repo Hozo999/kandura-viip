b6070a449433c1e6c56a0a9c9a21782a41dfb688
HOZOO DRAK99999 <162949416+Hozo999@users.noreply.github.com>
2024-03-28 15:44:31 +0700

    KANDURA-VIIP.sh

---
+++ KANDURA-VIIP.sh
@@ -0,0 +1,758 @@
+#!/data/data/com.termux/files/usr/bin/bash
+#hozoo
+clear
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+
+
+
+
+
+
+echo -e $red"
+                                      ⣀⣠⣤⡶⠶                ⠲⠶⣤⣤⣄⣀       
+                                  ⢀⣠⣶⣿⣿⠟⠉                     ⠙⠻⡿⣿⣦⣄    
+                                 ⣰⣿⣿⢏⡔⠁                         ⠘⢎⠻⣿⣷⡄  
+                                ⣰⣿⣻⠃⡞                            ⠈⡇⢹⣿⣿⡄ 
+                               ⢰⣿⣟⡗ ⡇                             ⣿⠐⣛⣿⣿ 
+                               ⢸⣿⣿⡓ ⢳⡀        ⣀⣀     ⠠⢄⣀⡀        ⣰⠇⠐⣻⣿⣿⡆
+                                ⢸⣿⣿⡷⠖⠋⠻⣄  ⣀⣤⠶⠚⠉⠁         ⠈⠙⠲⢦⣄⡀ ⢀⣴⠏⠈⠲⢿⣿⣿⠇
+                                ⠸⣿⣿⣿⣧⠞⠁⠈⠻⢾⣏                  ⠈⣻⡷⠋⡁⠈⢦⣾⣿⣿⣿ 
+                                 ⠹⣿⣿⣷⣷⡴⠃  ⡿                  ⣧  ⠱⣴⣷⣯⣿⡿⠃ 
+                                  ⠙⢿⣿⣯⣾⣿⢗⣼⠃                   ⠹⣦⢾⣿⣮⣿⣿⠟⠁  
+                                   ⣽⣿⣿⡿⠟⠁                     ⠘⠳⣽⣿⣿⡍    
+                                   ⢀⣿ ⣀                          ⢀⡠⢸⣇    
+                                   ⢸⡇ ⠘⣄                         ⡜  ⣿    
+                                  ⠘⣇ ⢄⣿⡰⡄                     ⡴⢸⣇ ⢀⡟    
+                                  ⣿ ⣾⣿⠇⠹⣶⣤⣀⣀ ⠙⢶⣤⡀   ⣠⣴⠖⠉⢀⣀⣠⣴⡾⠁⢿⣿⡆⢸⡇    
+                                   ⢸⣿⠏⢠⣾⣿⣿⣿⣿⣿⣦⡀⠹⡿  ⠸⡿⠁⣤⣾⣿⣿⣿⣿⣷⣦ ⢿⡇⡸     
+                                   ⢧⢿⡀⢸⣿⣿⣿⣿⣿⣿⣿⡟⠆     ⠞⣿⣿⣿⣿⣿⣿⣿⣿ ⣸⢧⠇     
+                                   ⠈⢈⡷⠈⢿⣿7⣿⣿⣿⣿⣿⡇  ⣠⣤⡀  ⣿⣿⣿⣿⣿⣿⣿⠃ ⣏⠈      
+                                   ⢀⡆   ⠙⠻⠿⠿⠿⠟⠁ ⢠⣿⣿⣧  ⠙⠿⠿⠿⠿⠛⠁   ⣆      
+                                    ⢸⡄          ⢀⣾⣿⢻⣿⣆           ⣸      
+                                    ⠘⢷⣄⡀        ⢾⣿⡿⢸⣿⣿⠆        ⣀⣴⡟      
+                                    ⠈⠙⠛⢛⣿⣿⣿⡖⠦⡀  ⠉⠁ ⠉⠁  ⢠⠖⣾⣿⣿⣿⠛⠛⠉       
+                                        ⠘⡇⣿⢻⣿⣴⣠⢀ ⡄ ⡀⢀⡄⢀⣀⣼⣼⣿⠹⡇⡟          
+                                       ⡀⠧⡇⢸⣿⣿⡇⢹⠒⡟⠙⡟⠉⡗⢹⠁⣿⣿⣿ ⡧⠇⡀         
+                                        ⢱  ⠘⢿⣹⠛⠼⣦⣿⣄⣧⣀⣷⣾⠴⢻⣸⠟  ⢠⡇         
+                                       ⠘⢧⡀  ⠊⠳⠧⣼⣠⣤⣧⣱⣸⡦⠷⠚⠃  ⣠⠟          
+                                          ⠈⠲⣤⡀   ⠈  ⠈    ⣠⡴⠋⠁           
+                                           ⠙⢿⣦        ⢀⣾⡟⠁             
+                                            ⠈⠻⢷⣄⣠⣴⣶⣤⣄⣰⠿⠋               
+                                                 ⠈⠉
+              "       
+
+
+echo -e $red"                            [ KUMPULAN KUMPULAN SCRIP TERMUX INDONESIA]"
+echo -e $green"
+[1].SPAM WA               [6].FIX SIGNAL 19 KALI LINUX  [11].UPDATE SCRIP HOZOO                [16].PANDORA BY MRSANZz
+[2].DDOS SILVER           [7].PHONEINFOGA OSINT         [12].LAPOR BUG SC ERROR HOZOO ?        [17].TOOLS4 BY GALIRUS 
+[3].DEFACE                [8].TRACK IP   OSINT          [13].YOUTUBE :SYSTEM UMBRELLA DRAK999  [18].TOOLS SPAM WA BY VIDRA 
+[4].SPAM CALL WA          [9].TRACK PHONE OSINT         [14].TIKTOK : SYSTEM UMBRELLA DRAK999  [19].PHIZHER LINK PISING 
+[5].INSTALLL KALI LINUX   [10].HACK PISING PIZHER       [15].TOOLS BANNER TERMUX               [20].CAM  PIZHER HACK 
+[21].SADAP CAMERA         [26].INSTALL UBUNTU KDE       [31].TOOLS 18 KALMENIX                 [36].TOOLS DESKTOP BY BYJHONZ
+[22].TOOLS owl oisnt      [27].INSTALL PKG MODULE TERMUX[32].TOOLS  🔰 DevilX 🔰               [37].TOOLS OSINT BY JHONZ
+[23].SPAM WA VIIP         [28].HACK CCTV                [33].TOOLS 🔰 WORDGEN 🔰               [38].TOOLS LACAK BY JHONZ
+[24].JARVIS BY GALIRUS    [29].TERMUX BACKUP            [34].TOOLS 🔰FACEBOOK HACK🔰           [39].TOOLS OSINT VIIP
+[25].VIRUS TIKTOK BY HOZOO[30].🔰 Facebook Bruteforce 🔰[35].🔰 LORDPHISH 🔰                   [40].TOOLS OSINT GhostTrack
+
+
+"
+read -p "                            SYSTEM UMBRELLA DRAK999-->" HOZOO;
+#Spam OTP
+if [ $HOZOO = 01 ] || [ $HOZOO = 1 ]
+    then
+    clear
+    echo -e $green" Installing..."
+    sleep 1
+apt update -y && apt upgrade -y
+apt install git -y
+apt install python -y
+git clone https://github.com/FatihArridho/brutal.git
+cd brutal
+pip install -r install.txt
+python brutal.py
+  clear
+    echo -e $green"Done Installed..."
+fi
+if [ $HOZOO = 02 ] || [ $HOZOO = 2 ]
+    then
+    clear
+    echo -e $green" Installing..."
+    sleep 1
+pip3 install requests pysocks
+git clone https://github.com/HC133/Pummel.git
+cd Pummel
+python3 pummel.py
+  clear
+    echo -e $green"Done Installed..."
+fi
+if [ $HOZOO = 03 ] || [ $HOZOO = 3 ]
+    then
+    clear
+    echo -e $green" Installing..."
+    sleep 1
+pkg install python2
+pip2 install requests
+pkg install git
+git clone https://github.com/B012ED/deface.git
+cd deface
+python2 def.py
+  clear
+    echo -e $green"Done Installed..."
+fi
+
+if [ $HOZOO = 04 ] || [ $HOZOO = 4 ]
+    then
+    clear
+    echo -e $green" Installing..."
+    sleep 1
+pkg update && pkg upgrade
+pkg install python
+pkg install python-pip
+pkg install python2
+pkg install git
+
+git clone https://github.com/Dra-ID/Prank-Call
+cd Prank-Call
+ls
+pip install inquirer
+pip install -r requirements.txt
+git pull
+python run.py
+  clear
+    echo -e $green"Done Installed..."
+fi
+
+if [ $HOZOO = 05 ] || [ $HOZOO = 5 ]
+    then
+    clear
+    echo -e $green" Installing..."
+    sleep 1
+termux-setup-storage
+ pkg install wget
+ wget -O install-nethunter-termux https://offs.ec/2MceZWr
+ chmod +x install-nethunter-termux
+ ./install-nethunter-termux
+  clear
+    echo -e $green"Done Installed..."
+fi
+
+if [ $HOZOO = 06 ] || [ $HOZOO = 6 ]
+    then
+    clear
+    echo -e $green" Installing..."
+    sleep 1
+pkg install git bash
+git clone http://github.com/Galirus404/GALIRUS-NETHUNTER
+cd GALIRUS-NETHUNTER
+bash G404.sh
+  clear
+    echo -e $green"Done Installed..."
+fi
+
+if [ $HOZOO = 07 ] || [ $HOZOO = 7 ]
+    then
+    clear
+    echo -e $green" Installing..."
+    sleep 1
+clear && echo -e '\033[1;32m[*] Download starting...' && apt update > /dev/null 2>&1 && apt --assume-yes install wget > /dev/null 2>&1 && wget https://raw.githubusercontent.com/ExpertAnonymous/PhoneInfoga/master/phoneinfoga.sh -q && clear && bash phoneinfoga.sh
+  clear
+    echo -e $green"Done Installed..."
+fi
+
+
+if [ $HOZOO = 08 ] || [ $HOZOO = 8 ]
+    then
+    clear
+    echo -e $green" Installing..."
+    sleep 1
+apt update ; apt install git curl -y ; git clone git://github.com/htr-tech/track-ip.git ; cd track-ip ; bash trackip
+  clear
+    echo -e $green"Done Installed..."
+fi
+if [ $HOZOO = 09 ] || [ $HOZOO = 9 ]
+    then
+    clear
+    echo -e $green" Installing..."
+    sleep 1
+git clone https://github.com/Theofilos-Chamalis/Find-Phone-Country.git
+
+
+cd Find-Phone-Country && npm install
+
+api/keys.ts
+npm run dev
+  clear
+    echo -e $green"Done Installed..."
+fi
+if [ $HOZOO = 10 ] || [ $HOZOO = 10 ]
+    then
+    clear
+    echo -e $green" Installing..."
+    sleep 1
+apt update ; apt install git -y ; git clone git://github.com/htr-tech/nexphisher.git ; cd nexphisher ; bash setup ; bash nexphisher
+  clear
+    echo -e $green"Done Installed..."
+fi
+if [ $HOZOO = 11 ] || [ $HOZOO = 11 ]
+    then
+    clear
+    echo -e $green" Installing..."
+    sleep 1
+xdg-open
+  clear
+    echo -e $green"Done Installed..."
+fi
+if [ $HOZOO = 12 ] || [ $HOZOO = 12 ]
+    then
+    clear
+    echo -e $green" Installing..."
+    sleep 1
+xdg-open https://www.tiktok.com/@cyber_hozoo_nethunter999?_t=8l2tpxccwny&_r=1
+  clear
+    echo -e $green"Done Installed..."
+fi
+if [ $HOZOO = 13 ] || [ $HOZOO = 13 ]
+    then
+    clear
+    echo -e $green" Installing..."
+    sleep 1
+xdg-open https://www.tiktok.com/@cyber_hozoo_nethunter999?_t=8l2tpxccwny&_r=1
+  clear
+    echo -e $green"Done Installed..."
+fi
+if [ $HOZOO = 14 ] || [ $HOZOO = 14 ]
+    then
+    clear
+    echo -e $green" Installing..."
+    sleep 1
+xdg-open https://www.tiktok.com/@cyber_hozoo_nethunter999?_t=8l2tpxccwny&_r=1
+  clear
+    echo -e $green"Done Installed..."
+fi
+if [ $HOZOO = 15 ] || [ $HOZOO = 15 ]
+    then
+    clear
+    echo -e $green" Installing..."
+    sleep 1
+pkg upgrade
+pkg update
+apt update
+apt upgrade
+ apt install python2 -y
+apt install git -y
+git clone https://github.com/h20-studio/T4MPILAN-V5
+cd T4MPILAN-V5
+ python2 style5.py
+  clear
+    echo -e $green"Done Installed..."
+fi
+if [ $HOZOO = 16 ] || [ $HOZOO = 16 ]
+    then
+    clear
+    echo -e $green" Installing..."
+    sleep 1
+apt update && apt upgrade && pkg update && pkg upgrade
+     pkg install curl
+     pkg install wget
+     pkg install python3
+     pkg install git
+     pkg install figlet
+     git clone https://github.com/MrSanZz/pandora
+     cd pandora
+     pip3 install -r requirements.txt
+     python3 pandora.py
+  clear
+    echo -e $green"Done Installed..."
+fi
+if [ $HOZOO = 17 ] || [ $HOZOO = 17 ]
+    then
+    clear
+    echo -e $green" Installing..."
+    sleep 1
+pkg update && pkg upgrade
+pkg install bash git -y
+git clone https://github.com/Galirus404/TOOLSV4
+cd TOOLSV4
+bash install.sh
+  clear
+    echo -e $green"Done Installed..."
+fi
+if [ $HOZOO = 18 ] || [ $HOZOO = 18 ]
+    then
+    clear
+    echo -e $green" Installing..."
+    sleep 1
+pkg update && pkg upgrade 
+pkg install python
+pkg install python-pip
+pkg install python2 
+pkg install git
+git clone https://github.com/Dra-ID/Brutal-Spam
+cd Brutal-Spam
+ls
+chmod 777 Brutal-Spam
+pip3 install inquirer 
+pip3 install -r requirements.txt
+git pull
+python run.py
+  clear
+    echo -e $green"Done Installed..."
+fi
+if [ $HOZOO = 19 ] || [ $HOZOO = 19 ]
+    then
+    echo -e $green" Installing..."
+   echo  -e $red "  COPY PASTE LINK GUSY UNTUK DI DOWNLOAND VIRTEX GANAS "
+   echo " https://www.mediafire.com/file/n8ayxky1b7qpiwb/Virtex+WA+Ganas.txt/file"
+ fi
+if [ $HOZOO = 20 ] || [ $HOZOO = 20 ]
+    then
+    clear
+    echo -e $green" Installing..."
+    sleep 1
+apt update ; apt install git -y ; git clone git://github.com/htr-tech/nexphisher.git ; cd nexphisher ; bash setup ; bash nexphisher
+  clear
+    echo -e $green"Done Installed..."
+fi
+if [ $HOZOO = 21 ] || [ $HOZOO = 21 ]
+    then
+    clear
+    echo -e $green" Installing..."
+    sleep 1
+termux-setup-storage
+apt update && apt upgrade -y
+apt install git -y
+apt install php
+apt install curl -y
+apt install wget -y
+git clone https://github.com/XPH4N70M/HACK-CAMERA.git
+cd HACK-CAMERA
+chmod +x hack_camera.sh 
+bash setup
+bash hack_camera.sh
+  clear
+    echo -e $green"Done Installed..."
+fi
+if [ $HOZOO = 22 ] || [ $HOZOO = 22 ]
+    then
+    clear
+    echo -e $green" Installing..."
+    sleep 1
+git clone https://github.com/IccTeam/Owl-sint
+cd Owl-sint
+bash module.sh
+python owlsint.py
+  clear
+    echo -e $green"Done Installed..."
+fi
+if [ $HOZOO = 23 ] || [ $HOZOO = 23 ]
+    then
+    clear
+    echo -e $green" Installing..."
+    sleep 1
+pkg update && pkg upgrade
+pkg install python
+pkg install python-pip
+pkg install python2
+pkg install git
+git clone https://github.com/Dra-ID/Wangsaff-Call
+cd Wangsaff-Call
+ls
+pip install inquirer
+pip install -r requirements.txt
+git pull
+python run.py
+  clear
+    echo -e $green"Done Installed..."
+fi
+if [ $HOZOO = 24 ] || [ $HOZOO = 24 ]
+    then
+    clear
+    echo -e $green" Installing..."
+    sleep 1
+pkg update && pkg upgrade
+pkg install git bash sox -y
+cd $HOME
+git clone --depth 1 https://github.com/Galirus404/JARVIS
+cd JARVIS
+bash jarvis.sh
+Selesai >_
+  clear
+    echo -e $green"Done Installed..."
+fi
+if [ $HOZOO = 25 ] || [ $HOZOO = 25 ]
+    then
+    clear
+    echo -e $green" Installing..."
+    sleep 1
+git clone https://github.com/Hozo999/tiktok
+cd tiktok
+chmod tiktok.sh
+bash tiktok.sh
+  clear
+    echo -e $green"Done Installed..."
+fi
+if [ $HOZOO = 26 ] || [ $HOZOO = 26 ]
+    then
+    clear
+    echo -e $green" Installing..."
+    sleep 1
+git clone https://github.com/T-Dynamos/ubuntu-termux
+cd ubuntu-termux
+chmod 777 ubuntu-termux
+python Ubuntu.py
+  clear
+    echo -e $green"Done Installed..."
+fi
+if [ $HOZOO = 27 ] || [ $HOZOO = 27 ]
+    then
+    clear
+    echo -e $green" Installing..."
+    sleep 1
+termux-setup-storage
+apt list 
+apt update 
+apt upgrade 
+pkg install python 
+pkg install python2 
+pkg install wget 
+pkg install git 
+pkg install php 
+pkg install nano 
+pkg install unzip 
+pip install requests
+pip install mechanize p
+pip install lolcat 
+pip install termcolor 
+pkg install root-repo 
+pkg install unstable-repo 
+pkg install x11-repo 
+pkg update && pkg upgrade 
+pkg install figlet 
+pkg install ruby 
+gem install lolcat 
+pkg install jq
+pip install bs4
+pkg install imagemagick
+pkg install nodejs
+pkg install ffmpeg
+pip install rich
+pip install requests
+pip install bs4
+termux-change-repo
+termux-setup-storage
+pkg update
+pkg upgrade
+pkg autoclean
+pkg install curl
+pkg install git
+pkg install wget
+pkg install zip
+pkg install unzip
+pkg Install python2
+pkg install python
+pkg install nodejs
+# LTS Version
+pkg install nodejs-lts
+pkg install jq
+pkg install libxml2-utils
+pkg install grep
+pkg install bc
+pkg install htop
+pkg install figlet
+pkg install httping
+pkg install dnsutils
+pkg install openssh
+pkg install ffmpeg   
+pkg install termux-create-package
+pkg install proot-distro
+pkg install clang
+pkg install libffi
+pkg install openssl
+pkg install screenfetch
+pkg install w3m
+pkg install cowsay
+pkg install perl
+pkg install ruby
+pkg install rust
+pkg install Cloudflared
+pkg install sqlite
+pkg install fakeroot
+pkg install sshpass
+pkg install golang
+pkg install proot
+pkg install tigervnc xorg-xhost
+pkg install x11-repo
+pkg update
+pkg install root-repo
+pkg update -y
+pkg upgrade -y
+pkg install php -y
+pkg install openssh -y
+pkg install git -y
+git install github -y
+pkg install figlet -y
+pkg install python -y
+pkg install python2 -y
+pkg install python3 -y
+pkg install perl -y
+pkg install ruby -y
+pkg install wget -y
+pkg install nano -y
+pkg install curl -y
+pkg install x11-repo
+pkg install tigervnc
+apt install php -y
+apt install python -y
+apt install python2 -y
+apt install python3 -y
+apt install nano -y
+apt install toilet -y
+apt install figlet -y
+apt install wget -y
+apt install wcalc -y 
+apt install nmap -y
+apt install curl -y
+apt install bmon -y
+apt install apache2 -y
+apt install ruby -y
+apt install tor -y
+apt install openssl -y
+apt install macchanger -y
+apt install install perl -y
+apt install unzip -y
+apt install python3 python3-pip
+apt install curl && \
+export install_path="/usr/local/bin" && \
+mkdir -p "$install_path" && \
+ curl -L 'https://github.com/termux/termux-create-package/releases/latest/download/termux-create-package' -o "$install_path/termux-create-package" && \
+ chmod 755 "$install_path/termux-create-package";
+export install_path="/usr/local/bin"
+mkdir -p "$install_path"
+pip install ruamel.yaml
+pip3 install ruamel.yaml.
+pip install --upgrade pip setuptools
+pip install --upgrade httpie
+pip install -U requests[socks]
+pip install requests
+  clear
+    echo -e $green"Done Installed..."
+fi
+if [ $HOZOO = 28 ] || [ $HOZOO = 28 ]
+    then
+    clear
+    echo -e $green" Installing..."
+    sleep 1
+pkg install update
+pkg install upgrade
+pkg install python 
+pkg install python2
+pkg install python3
+pkg install python4
+pkg install python5
+pkg install python6
+pkg install python7
+pkg install python8
+pkg install python9
+pkg install python10
+pkg install python11
+pkg install python12
+pkg install python13
+pkg install python14
+pkg install python15
+pkg install python16
+pkg install python17
+pkg install python18
+pkg install python19
+pkg install python20
+pkg install git
+ git clone https://github.com/AngelSecurityTeam/Cam-Hackers
+ cd Cam-Hackers
+
+ pip install colorama 
+ python3 cam-hackers.py
+  clear
+    echo -e $green"Done Installed..."
+fi
+if [ $HOZOO = 29 ] || [ $HOZOO = 29 ]
+    then
+    clear
+    echo -e $green" Installing..."
+    sleep 1
+pkg install git -y
+ git clone https://github.com/Dark-Legends/Terminal-backup
+ cd Terminal-backup
+chmod +x terminal-backup 
+ ./terminal-backup
+  clear
+    echo -e $green"Done Installed..."
+fi
+if [ $HOZOO = 30 ] || [ $HOZOO = 30 ]
+    then
+    clear
+    echo -e $green" Installing..."
+    sleep 1
+apt update && apt upgrade -y
+apt install git -y
+ git clone https://github.com/hackerxphantom/Facebook_hack.git
+ cd Facebook_hack
+bash setup
+python facebook_hack.py
+  clear
+    echo -e $green"Done Installed..."
+fi
+if [ $HOZOO = 31 ] || [ $HOZOO = 31 ]
+    then
+    clear
+    echo -e $green" Installing..."
+    sleep 1
+apt-get update -y
+apt-get upgrade -y
+apt-get install git -y
+ git clone https://github.com/MrHacker-X/KalnemiX.git
+cd KalnemiX
+ chmod +x *
+ bash setup.sh
+ python kalnemix.py
+  clear
+    echo -e $green"Done Installed..."
+fi
+if [ $HOZOO = 32 ] || [ $HOZOO = 32 ]
+    then
+    clear
+    echo -e $green" Installing..."
+    sleep 1
+apt update -y
+apt upgrade -y
+apt install git -y
+git clone https://github.com/MrHacker-X/DevilX.git
+cd DevilX
+chmod +x *
+bash setup.sh
+python devilx.py
+  clear
+    echo -e $green"Done Installed..."
+fi
+if [ $HOZOO = 33 ] || [ $HOZOO = 33 ]
+    then
+    clear
+    echo -e $green" Installing..."
+    sleep 1
+apt update && apt upgrade
+ apt install git
+ apt install python    
+git clone https://github.com/vpphacker/wordgen.git
+cd wordgen
+ chmod +x *
+ sh install.sh    
+python3 wordgen.py
+  clear
+    echo -e $green"Done Installed..."
+fi
+if [ $HOZOO = 34 ] || [ $HOZOO = 34 ]
+    then
+    clear
+    echo -e $green" Installing..."
+    sleep 1
+apt update && apt upgrade
+pkg install python
+pkg install git
+git clone https://github.com/STARK-404/FB-HACK/
+cd FB-HACK 
+pip install -r requirments.txt
+python Run.py
+  clear
+    echo -e $green"Done Installed..."
+fi
+if [ $HOZOO = 35 ] || [ $HOZOO = 35 ]
+    then
+    clear
+    echo -e $green" Installing..."
+    sleep 1
+apt-get update -y
+apt-get install php -y
+apt-get install openssh -y 
+apt-get install wget -y
+apt-get install git -y
+git clone https://github.com/Black-Hell-Team/LordPhish.git
+cd LordPhish
+bash setup.sh 
+bash lord.sh
+  clear
+    echo -e $green"Done Installed..."
+fi
+if [ $HOZOO = 36 ] || [ $HOZOO = 36 ]
+    then
+    clear
+    echo -e $green" Installing..."
+    sleep 1
+pkg update
+pkg upgrade
+pkg install git
+git clone https://github.com/MR-JHONz/DESKTOP-TERMUX-LINUX
+cd DESKTOP-TERMUX-LINUX
+bash GUI-BYJHONZ.sh
+  clear
+    echo -e $green"Done Installed..."
+fi
+if [ $HOZOO = 37 ] || [ $HOZOO = 37 ]
+    then
+    clear
+    echo -e $green" Installing..."
+    sleep 1
+pkg update && pkg upgrade -y
+pip install colorama
+pkg install git -y
+git clone https://github.com/MR-JHONz/OsintTSC.git
+cd OsintTSC
+bash TSC_OSINT.sh
+  clear
+    echo -e $green"Done Installed..."
+fi
+if [ $HOZOO = 38 ] || [ $HOZOO = 38 ]
+    then
+    clear
+    echo -e $green" Installing..."
+    sleep 1
+pkg update
+pkg upgrade
+pkg install python
+pip install requests
+pkg install git
+git clone https://github.com/MR-JHONz/CYBER_TASIK.git
+cd CYBER_TASIK
+python lacaklinux.py
+  clear
+    echo -e $green"Done Installed..."
+fi 
+if [ $HOZOO = 39 ] || [ $HOZOO = 39 ]
+    then
+    clear
+    echo -e $green" Installing..."
+    sleep 1
+cd $HOME
+git clone https://github.com/TermuxHackz/X-osint
+cd X-osint
+chmod +x *
+bash setup.sh
+  clear
+    echo -e $green"Done Installed..."
+fi
+if [ $HOZOO = 40 ] || [ $HOZOO = 40 ]
+    then
+    clear
+    echo -e $green" Installing..."
+    sleep 1
+git clone https://github.com/HunxByts/GhostTrack.git
+cd GhostTrack
+pip3 install -r requirements.txt
+python3 GhostTR.py
+  clear
+    echo -e $green"Done Installed..."
+fi
